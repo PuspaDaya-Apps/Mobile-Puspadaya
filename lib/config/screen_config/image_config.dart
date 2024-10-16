c2e64cdd4ff5cb1fa810a08dad9be8d3a0a9cbfc
arilex <arilex36@gmail.com>
2024-10-16 20:02:36 +0700

    tambah gambar vector error handler

--- lib/config/screen_config/image_config.dart
+++ lib/config/screen_config/image_config.dart
@@ -1,1 +1,7 @@
 //! contoh variable "const String exampleName = 'assets/images/example.jpg';"
+
+const String noDataVector = 'assets/images/no_data_vector.png';
+const String dataNotFoundVector = 'assets/images/data_not_found_vector.png';
+const String logoutVector = 'assets/images/logout_vector.png';
+const String lostConnectionVector = 'assets/images/lost_connection_vector.png';
+const String pageNotFoundVector = 'assets/images/page_not_found_vector.png';
--- lib/config/theme/pallet_color.dart
+++ lib/config/theme/pallet_color.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 //! contoh variabel 'const Color exampleColor = Color(0xFFFFFFFF);'
+
+const Color fontColor1 = Color(0xFF181D27);
