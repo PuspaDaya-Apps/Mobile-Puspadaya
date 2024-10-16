f3c50c2a4da1650aa87334ea534581f6c5109bbb
arilex <arilex36@gmail.com>
2024-10-16 15:24:10 +0700

    pengelompokan folder

--- lib/route/route_page.dart
+++ lib/route/route_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import './route_name.dart';
 
-import '../view/screen/page_not_found_screen.dart';
-import '../view/screen/splash_screen.dart';
+import '../app/view/screen/page_not_found_screen.dart';
+import '../app/view/screen/splash_screen.dart';
 
 class MyRoute {
   static Route<dynamic> generateRoute(RouteSettings settings) {
