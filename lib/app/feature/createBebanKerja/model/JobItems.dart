913688d145d741f604ad5995750fc6a496b3c461
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-01-25 20:23:41 +0700

    feat: fixig create beban kewajiban

--- lib/app/feature/createBebanKerja/model/JobItems.dart
+++ lib/app/feature/createBebanKerja/model/JobItems.dart
@@ -2,9 +2,11 @@ class Jobitems {
   bool status;
   String label;
   List<Jobitems>? subItems;
+  bool? isExpanded;
   Jobitems({
     required this.label,
     required this.status,
+    this.isExpanded,
     this.subItems, // Menambahkan parameter opsional untuk sub-items
   });
 }
