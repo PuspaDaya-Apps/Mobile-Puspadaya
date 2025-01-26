8b307c467f8b8a60e9cf03fa35d8ac90f9964cf6
arilex <arilex36@gmail.com>
2025-01-26 14:27:28 +0700

    create widget card untuk register

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../config/screen_config/image_config.dart';
 import '../../../config/screen_config/size_config.dart';
 import '../../../config/theme/pallet_color.dart';
 import '../../../route/route_name.dart';
+import '../../feature/authorization/bloc/blocAuthentication/authentication_bloc.dart';
+import '../../feature/authorization/bloc/blocAuthorization/authorization_bloc.dart';
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
@@ -13,50 +16,61 @@ class SplashScreen extends StatefulWidget{
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3)).then((value){
-      Navigator.pushReplacementNamed(context, ONBOARDING);
+    Future.delayed(const Duration(seconds: 3)).then((value) async {
+      BlocProvider.of<AuthenticationBloc>(context).add(AppStartEvent());
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: baseColor,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          Center(
-            child: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: SizeConfig.calWidthMultiplier(97.5),
-              child: Image.asset(
-                logoText,
-                width: SizeConfig.calMultiplierImage(125),
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: EdgeInsets.only(
-                bottom: SizeConfig.calHeightMultiplier(25) 
-              ),
-              child: Text(
-                "V. 1.0",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: SizeConfig.calMultiplierText(18),
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        if(state is AuthenticationFirstTime) {
+          Navigator.pushNamed(context, ONBOARDING);
+        }
+        if(state is AuthenticationTrue) {
+          BlocProvider.of<AuthorizationBloc>(context).add(AuthorizationTrueEvent());
+          Navigator.pushNamed(context, HOMEEXAMPLE);
+        }
+        if(state is AuthenticationFalse) {
+          Navigator.pushNamed(context, LOGIN);
+        }
+      },
+      child: Scaffold(
+        backgroundColor: baseColor,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: SizeConfig.calWidthMultiplier(97.5),
+                child: Image.asset(
+                  logoText,
+                  width: SizeConfig.calMultiplierImage(125),
                 ),
               ),
             ),
-          )
-        ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding:
+                    EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
+                child: Text(
+                  "V. 1.0",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: SizeConfig.calMultiplierText(18),
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
