d67efe2ba8a4510c43b4746b6e01c3f17fc0a1c9
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-02-02 11:44:03 +0700

    fix: implement api for inde and detail register orang tua

--- lib/app/feature/createRegisterAnak/view/search_kk.dart
+++ lib/app/feature/createRegisterAnak/view/search_kk.dart
@@ -5,23 +5,25 @@ import '../../../../config/theme/pallet_color.dart';
 import '../../../view/widget/appbar_widget.dart';
 import '../../../view/widget/card_orangtua_widget.dart';
 import '../../../view/widget/search_text_field_widget.dart';
-import '../../registerOrangTua/view/model/orang_tua_item_model.dart';
+import '../../registerOrangTua/model/orang_tua_item_model.dart';
 
 class SearchKartuKeluarga extends StatelessWidget {
   TextEditingController _searchController = TextEditingController();
 
-  List<OrangTuaItemModel> listOrangTua = [
-    OrangTuaItemModel(
-        husband: 'Heri Dharmawan',
-        wife: 'Tari Saputri',
-        initial: 'HD',
-        kk: '62080635261527'),
-    OrangTuaItemModel(
-        husband: 'Hermawan',
-        wife: 'Nensiana Puji Astuti',
-        initial: 'HM',
-        kk: '62080723287167'),
-  ];
+  // List<OrangTuaItemModel> listOrangTua = [
+  //   OrangTuaItemModel(
+  //       husband: 'Heri Dharmawan',
+  //       wife: 'Tari Saputri',
+  //       initial: 'HD',
+  //       kk: '62080635261527',
+  //       id: '1'),
+  //   OrangTuaItemModel(
+  //       husband: 'Hermawan',
+  //       wife: 'Nensiana Puji Astuti',
+  //       initial: 'HM',
+  //       kk: '62080723287167',
+  //       id: '2'),
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,26 +77,26 @@ class SearchKartuKeluarga extends StatelessWidget {
               SizedBox(
                 height: 12,
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: listOrangTua.length,
-                  itemBuilder: (context, index) {
-                    OrangTuaItemModel orangTua = listOrangTua[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: CardOrangtuaWidget(
-                        onTap: () {
-                          Navigator.pop(context, orangTua);
-                        },
-                        kk: orangTua.kk,
-                        namaAyah: orangTua.husband,
-                        namaIbu: orangTua.wife,
-                        profile: orangTua.initial,
-                      ),
-                    );
-                  },
-                ),
-              )
+              // Expanded(
+              //   child: ListView.builder(
+              //     itemCount: listOrangTua.length,
+              //     itemBuilder: (context, index) {
+              //       OrangTuaItemModel orangTua = listOrangTua[index];
+              //       return Padding(
+              //         padding: const EdgeInsets.only(bottom: 10),
+              //         child: CardOrangtuaWidget(
+              //           onTap: () {
+              //             Navigator.pop(context, orangTua);
+              //           },
+              //           kk: orangTua.kk,
+              //           namaAyah: orangTua.husband,
+              //           namaIbu: orangTua.wife,
+              //           profile: orangTua.initial,
+              //         ),
+              //       );
+              //     },
+              //   ),
+              // )
             ],
           ),
         ),
--- lib/app/feature/registerPengasuh/view/model/pengasuh_item_model.dart
+++
@@ -1,7 +0,0 @@
-class PengasuhItemModel {
-  final String name;
-  final String nik;
-  final String child;
-  PengasuhItemModel(
-      {required this.name, required this.nik, required this.child});
-}
