0b7171f4db8888aa03773b4344cd8e0df9ea1ab8
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-02-02 11:44:03 +0700

    fix: implement api for inde and detail register orang tua

--- lib/app/feature/createRegisterAnak/view/create_register_anak.dart
+++ lib/app/feature/createRegisterAnak/view/create_register_anak.dart
@@ -16,7 +16,7 @@ import '../../../view/widget/outline_button_widget.dart';
 import '../../../view/widget/primary_button_widget.dart';
 import '../../../view/widget/textField_widget.dart';
 import '../../createPengukuranIbuHamil/view/create_pengukuran_ibu_hamil.dart';
-import '../../registerOrangTua/view/model/orang_tua_item_model.dart';
+import '../../registerOrangTua/model/orang_tua_item_model.dart';
 import '../cubit/search_kk_cubit.dart';
 import 'search_kk.dart';
 
