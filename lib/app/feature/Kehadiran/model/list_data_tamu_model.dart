cc106cc5d0c49b15024ebe05d86f316a2e81a71d
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-02-05 21:11:17 +0700

    fix: fixing kehadiran

---
+++ lib/app/feature/Kehadiran/model/list_data_tamu_model.dart
@@ -0,0 +1,13 @@
+class ListDataTamuModel {
+  String? nama;
+  String? posyanduAsal;
+  String? namaPosyandu;
+  String? alamat;
+
+  ListDataTamuModel({
+    this.nama,
+    this.posyanduAsal,
+    this.namaPosyandu,
+    this.alamat,
+  });
+}
