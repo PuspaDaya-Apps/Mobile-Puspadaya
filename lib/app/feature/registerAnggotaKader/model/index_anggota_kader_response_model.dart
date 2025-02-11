b73bf876f9c357d7c99554e77f114a58eb66f7c5
arilex <arilex36@gmail.com>
2025-02-11 10:08:03 +0700

    index anggota kader

---
+++ lib/app/feature/registerAnggotaKader/model/index_anggota_kader_response_model.dart
@@ -0,0 +1,113 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../../utils/helper/helper_core.dart';
+
+// part 'index_anggota_kader_response_model.g.dart';
+
+// @JsonSerializable()
+class IndexAnggotaKaderResponseModel {
+  String message;
+  String? error;
+  List<Data>? data = [];
+  Meta? meta;
+
+  IndexAnggotaKaderResponseModel({
+    required this.message,
+    this.error,
+    this.data,
+    this.meta
+  });
+
+  factory IndexAnggotaKaderResponseModel.fromJson(Map<String, dynamic> json) => _$IndexAnggotaKaderResponseModelFromJson(json);
+
+  Map<String, dynamic> toJson()=>_$IndexAnggotaKaderResponseModelToJson(this);
+}
+
+// @JsonSerializable()
+class Data {
+  String id;
+  String avatar;
+  @JsonKey(name: 'nama_lengkap')
+  String namaLengkap;
+  String? email;
+
+  Data({
+    required this.id,
+    required this.avatar,
+    required this.namaLengkap,
+    this.email,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) => _$DataFromJson(json);
+
+  Map<String, dynamic> toJson()=>_$DataToJson(this);
+}
+
+// @JsonSerializable()
+class Meta {
+  int itemsPerPage;
+  int totalItems;
+  int currentPage;
+  int totalPages;
+
+  Meta({
+    required this.itemsPerPage,
+    required this.totalItems,
+    required this.currentPage,
+    required this.totalPages
+  });
+
+  factory Meta.fromJson(Map<String, dynamic> json) => _$MetaFromJson(json);
+
+  Map<String, dynamic> toJson()=>_$MetaToJson(this);
+}
+
+IndexAnggotaKaderResponseModel _$IndexAnggotaKaderResponseModelFromJson(
+        Map<String, dynamic> json) =>
+    IndexAnggotaKaderResponseModel(
+      message: json['message'] as String,
+      error: json['error'] as String?,
+      data: (json['data'] as List<dynamic>?)
+          ?.map((e) => Data.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      meta: json['meta'] == null
+          ? null
+          : Meta.fromJson(json['meta'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$IndexAnggotaKaderResponseModelToJson(
+        IndexAnggotaKaderResponseModel instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+      'error': instance.error,
+      'data': instance.data,
+      'meta': instance.meta,
+    };
+
+Data _$DataFromJson(Map<String, dynamic> json) => Data(
+      id: json['id'] as String,
+      avatar: HelperCore.convertNameToInitial(json['nama_lengkap']) ,
+      namaLengkap: json['nama_lengkap'] as String,
+      email: json['email'] as String?,
+    );
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'id': instance.id,
+      'avatar': instance.avatar,
+      'nama_lengkap': instance.namaLengkap,
+      'email': instance.email,
+    };
+
+Meta _$MetaFromJson(Map<String, dynamic> json) => Meta(
+      itemsPerPage: (json['itemsPerPage'] as num).toInt(),
+      totalItems: (json['totalItems'] as num).toInt(),
+      currentPage: (json['currentPage'] as num).toInt(),
+      totalPages: (json['totalPages'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$MetaToJson(Meta instance) => <String, dynamic>{
+      'itemsPerPage': instance.itemsPerPage,
+      'totalItems': instance.totalItems,
+      'currentPage': instance.currentPage,
+      'totalPages': instance.totalPages,
+    };
--- lib/app/feature/registerAnggotaKader/view/register_anggota_kader.dart
+++ lib/app/feature/registerAnggotaKader/view/register_anggota_kader.dart
@@ -1,11 +1,15 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:puspadaya/app/view/widget/card_anggotakader_widget.dart';
 import 'package:puspadaya/route/route_name.dart';
 
 import '../../../../config/theme/pallet_color.dart';
+import '../../../view/screen/error_server_screen.dart';
+import '../../../view/screen/no_data_screen.dart';
 import '../../../view/widget/appbar_widget.dart';
 import '../../../view/widget/search_text_field_widget.dart';
+import '../bloc/index_anggota_kader_bloc.dart';
 import 'model/anggota_kader_item_model.dart';
 
 class RegisterAnggotaKader extends StatelessWidget {
@@ -13,7 +17,10 @@ class RegisterAnggotaKader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RegisterAnggotaKaderView();
+    return BlocProvider(
+      create: (context) => IndexAnggotaKaderBloc(),
+      child: const RegisterAnggotaKaderView(),
+    );
   }
 }
 
@@ -28,20 +35,30 @@ class RegisterAnggotaKaderView extends StatefulWidget {
 class _RegisterAnggotaKaderViewState extends State<RegisterAnggotaKaderView> {
   TextEditingController _searchController = TextEditingController();
 
-  List<AnggotaKaderItemModel> listKader = [
-    AnggotaKaderItemModel(
-      name: 'Muhammad Kaivan Al Hakim',
-      email: 'kaivan@gmail.com',
-      initial: 'MK',
-    ),
-    AnggotaKaderItemModel(
-      name: 'Intan wahyuni Soraya',
-      email: 'intan@gmail.com',
-      initial: 'IW',
-    ),
-  ];
+  // List<AnggotaKaderItemModel> listKader = [
+  //   AnggotaKaderItemModel(
+  //     name: 'Muhammad Kaivan Al Hakim',
+  //     email: 'kaivan@gmail.com',
+  //     initial: 'MK',
+  //   ),
+  //   AnggotaKaderItemModel(
+  //     name: 'Intan wahyuni Soraya',
+  //     email: 'intan@gmail.com',
+  //     initial: 'IW',
+  //   ),
+  // ];
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<IndexAnggotaKaderBloc>(context)
+        .add(GetListAnggotaKaderEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
+    final indexAnggotKaderBloc =
+        BlocProvider.of<IndexAnggotaKaderBloc>(context);
+
     return Scaffold(
       appBar: PrimaryAppBar(
         title: 'Kelola  Anggota Kader',
@@ -59,63 +76,90 @@ class _RegisterAnggotaKaderViewState extends State<RegisterAnggotaKaderView> {
           color: Colors.white,
         ),
         onPressed: () {
-          Navigator.pushNamed(context, CREATE_REGISTER_ANGGOTA_KADER);
+          Navigator.pushNamed(context, CREATE_REGISTER_ANGGOTA_KADER)
+              .then((value) {
+            if (value != null) {
+              indexAnggotKaderBloc.add(GetListAnggotaKaderEvent());
+            }
+          });
           // Add your navigation or functionality for adding new items
-          print("Floating Action Button Pressed");
+          // print("Floating Action Button Pressed");
         },
       ),
       backgroundColor: backgroundWhite10,
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                spacing: 10,
-                children: [
-                  Expanded(
-                    child: SearchTextFieldWidget(
-                      controller: _searchController,
-                      hintText: 'Cari Data Anak',
-                    ),
+          child: BlocConsumer<IndexAnggotaKaderBloc, IndexAnggotaKaderState>(
+            listener: (context, state) {
+              debugPrint(state.toString());
+            },
+            builder: (context, state) {
+              if(state is IndexAnggotaKaderProcessState) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: bluePrimaryMain,
                   ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Icon(
-                      size: 30,
-                      FluentIcons.filter_24_filled,
+                );
+              }
+              if(state is IndexAnggotaKaderSuccessState) {
+                if(state.indexAnggotaKaderResponseModel.data!.isEmpty) {
+                  return const NoDataScreen();
+                }
+                return Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      spacing: 10,
+                      children: [
+                        Expanded(
+                          child: SearchTextFieldWidget(
+                            controller: _searchController,
+                            hintText: 'Cari Data Anak',
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Icon(
+                            size: 30,
+                            FluentIcons.filter_24_filled,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: listKader.length,
-                  itemBuilder: (context, index) {
-                    AnggotaKaderItemModel anggotaKader = listKader[index];
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
-                      child: CardAnggotakaderWidget(
-                        onTap: () {
-                          Navigator.pushNamed(
-                            context,
-                            DETAIL_REGISTER_ANGGOTA_KADER,
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: state.indexAnggotaKaderResponseModel.data!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 10),
+                            child: CardAnggotakaderWidget(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  DETAIL_REGISTER_ANGGOTA_KADER,
+                                  arguments: state.indexAnggotaKaderResponseModel.data![index].id
+                                );
+                              },
+                              email: state.indexAnggotaKaderResponseModel.data![index].email == null 
+                              ? "Tidak memiliki email"
+                              : state.indexAnggotaKaderResponseModel.data![index].email!,
+                              profile: state.indexAnggotaKaderResponseModel.data![index].avatar,
+                              nama: state.indexAnggotaKaderResponseModel.data![index].namaLengkap,
+                            ),
                           );
                         },
-                        email: anggotaKader.email,
-                        profile: anggotaKader.initial,
-                        nama: anggotaKader.name,
                       ),
-                    );
-                  },
-                ),
-              )
-            ],
+                    )
+                  ],
+                );
+              }
+              return const ErrorServerScreen();
+            },
           ),
         ),
       ),
