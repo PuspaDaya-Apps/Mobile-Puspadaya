79d2d3965ee5008d0882484bb476fde091e005bc
arilex <arilex36@gmail.com>
2025-02-11 20:24:28 +0700

    persiapan fitur lupa kata sandi

---
+++ lib/app/feature/login/bloc/lupaKataSandiBloc/lupa_kata_sandi_state.dart
@@ -0,0 +1,10 @@
+part of 'lupa_kata_sandi_bloc.dart';
+
+sealed class LupaKataSandiState extends Equatable {
+  const LupaKataSandiState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class LupaKataSandiInitial extends LupaKataSandiState {}
--- lib/app/feature/login/service/login_api.dart
+++ lib/app/feature/login/service/login_api.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import '../../../../utils/api_utils/api_utils.dart';
 import '../../../../utils/network_utils/network_utils.dart';
 import '../model/login_model.dart';
+import '../model/lupa_kata_sandi_model.dart';
 
 class LoginApi {
   Future<List<dynamic>> loginService (LoginModel loginModel) async {
@@ -23,6 +24,17 @@ class LoginApi {
     final Uri link = ApiUtils().urlCurrentUser();
 
     return await NetworkUtils().get(link, header).then((response) {
+      debugPrint(response.toString());
+      return response;
+    });
+  }
+
+  Future<List<dynamic>> lupaKataSandiService (String token, LupaKataSandiModel lupaKataSandiModel) async {
+    final Map<String, String> header = ApiUtils().header();
+    final Uri link = ApiUtils().urlForgotPassword();
+    final String body = jsonEncode(lupaKataSandiModel.toJson()); 
+
+    return await NetworkUtils().post(link, header, body).then((response) {
       debugPrint(response.toString());
       return response;
     });
