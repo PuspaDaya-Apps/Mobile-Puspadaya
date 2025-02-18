96a8c431ab74ae94b4fa5d1cd8e926180aa046ed
arilex <arilex36@gmail.com>
2025-02-18 12:17:42 +0700

    ganti http dengan dio

--- lib/utils/network_utils/network_utils.dart
+++ lib/utils/network_utils/network_utils.dart
@@ -2,44 +2,148 @@ import "dart:async";
 import "dart:convert";
 
 import "package:flutter/material.dart";
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import "package:puspadaya/utils/shared_preferences_utils/shared_preferences_utils.dart";
+
+import "../../app/model/refreshtoken_model.dart";
+import "../api_utils/api_utils.dart";
 // import "package:image_picker/image_picker.dart";
 
-class NetworkUtils {
-  Future<List<dynamic>> get(Uri url, Map<String, String> header) async {
-    return http.get(url, headers: header).then((response) {
-      final String bodyResponse = response.body;
-      final int statusResponse = response.statusCode;
 
-      debugPrint(bodyResponse);
+class NetworkUtils {
+  String? token;
+  late Dio dio;
+
+  NetworkUtils({
+    this.token
+  }) {
+    dio = Dio(
+      BaseOptions(
+        contentType: 'application/json',
+        responseType: ResponseType.json,
+        validateStatus: (status) => true        
+      )
+    );
+  
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        debugPrint('Set Header');
+        //! header
+        if(token == null) {
+          debugPrint('header no token');
+          options.headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json'
+          };
+        } else {
+          debugPrint('header token');
+          // options.headers['Authorization'] = 'Bearer $token';
+          options.headers = { 
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          };
+        }
+        debugPrint('send header');
+        handler.next(options);
+      },
+      onError: (DioException e, handler) async {
+        debugPrint('something error');
+        if (e.response?.statusCode == 401) {
+          debugPrint('access token refresh');
+          // await _handleUnauthorizedError();
+        }
+      },
+    ));
+  }
+
+  Future<List<dynamic>> refreshToken () async {
+    String? refreshTokenValue = await SharedPrefUtils().getRefreshToken();
+
+    if(refreshTokenValue != null) {
+      RefreshTokenModel refreshTokenModel = RefreshTokenModel.fromJson(json.decode(refreshTokenValue));
+
+      try {
+        final response = await dio.post(
+          ApiUtils().urlRefreshToken(),
+          data: {
+            "refresh_token" : refreshTokenModel.refreshToken
+          }
+        );
+
+          final bodyResponse = response.data;
+          final int statusResponse = response.statusCode!;
+
+          debugPrint(bodyResponse.toString());
+          debugPrint(statusResponse.toString());
+
+          if (statusResponse == 200 ||
+              statusResponse == 201 ||
+              statusResponse == 202 ||
+              statusResponse == 206 ||
+              statusResponse == 401 ||
+              statusResponse == 403 ||
+              statusResponse == 404 ||
+              statusResponse == 400 ||
+              statusResponse == 422 ||
+              statusResponse == 409) {
+          return [statusResponse, json.decode(json.encode(bodyResponse))];
+        } else {
+          return [statusResponse, json.decode(json.encode(bodyResponse))];
+        }
+      } on DioException catch (e) {
+        throw e.response!.statusMessage.toString();
+      }
+    } else {
+      return [401, <String, dynamic> {
+        "message": "Token tidak dapat ditemukan\nHarap login kembali"
+      }];
+    }
+  }
+
+  Future<List<dynamic>> get (String url, Map<String, dynamic> parameterQuery) async {
+    try {
+      final response = await dio.get(
+        url,
+        queryParameters: parameterQuery
+      );
+
+      final bodyResponse = response.data;
+      final int statusResponse = response.statusCode!;
+
+      debugPrint(bodyResponse.toString());
       debugPrint(statusResponse.toString());
 
       if (statusResponse == 200 ||
           statusResponse == 201 ||
           statusResponse == 202 ||
           statusResponse == 206 ||
-          statusResponse == 401 ||
           statusResponse == 403 ||
           statusResponse == 404 ||
           statusResponse == 400) {
-        return [statusResponse, json.decode(bodyResponse)];
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
       } else {
-        throw bodyResponse;
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
       }
-    }).catchError((dynamic error) {
-      throw Exception(error);
-    });
+    } on DioException catch (e) {
+      throw e.response!.statusMessage.toString();
+    }
   }
 
-  Future<List<dynamic>> post(
-      Uri url, Map<String, String> header, String body) async {
-    return http
-        .post(url, headers: header, body: body)
-        .then((http.Response response) {
-      final String bodyResponse = response.body;
-      final int statusResponse = response.statusCode;
+  Future<List<dynamic>> post (String url, String body) async {
+    debugPrint("start connection");
+    try {
+      final response = await dio.post(
+        url,
+        data: body
+      );
+      debugPrint(" procces connection");
 
-      debugPrint(bodyResponse);
+      final bodyResponse = response.data;
+      final int statusResponse = response.statusCode!;
+
+      debugPrint(bodyResponse.toString());
       debugPrint(statusResponse.toString());
 
       if (statusResponse == 200 ||
@@ -52,13 +156,107 @@ class NetworkUtils {
           statusResponse == 400 ||
           statusResponse == 422 ||
           statusResponse == 409) {
-        return [statusResponse, json.decode(bodyResponse)];
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
       } else {
-        throw bodyResponse;
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
       }
-    }).catchError((dynamic error) {
-      throw Exception(error);
-    });
+    } on DioException catch (e) {
+      debugPrint('error exception');
+      throw e.response!.statusMessage.toString();
+    }
+  }
+
+  Future<List<dynamic>> put (String url, String body) async {
+    try {
+      final response = await dio.put(
+        url,
+        data: body
+      );
+
+        final bodyResponse = response.data;
+        final int statusResponse = response.statusCode!;
+
+        debugPrint(bodyResponse.toString());
+        debugPrint(statusResponse.toString());
+
+        if (statusResponse == 200 ||
+            statusResponse == 201 ||
+            statusResponse == 202 ||
+            statusResponse == 206 ||
+            statusResponse == 401 ||
+            statusResponse == 403 ||
+            statusResponse == 404 ||
+            statusResponse == 400 ||
+            statusResponse == 422 ||
+            statusResponse == 409) {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      } else {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      }
+    } on DioException catch (e) {
+      throw e.response!.statusMessage.toString();
+    }
+  }
+
+  Future<List<dynamic>> patch (String url, String body) async {
+    try {
+      final response = await dio.patch(
+        url,
+        data: body
+      );
+
+        final bodyResponse = response.data;
+        final int statusResponse = response.statusCode!;
+
+        debugPrint(bodyResponse.toString());
+        debugPrint(statusResponse.toString());
+
+        if (statusResponse == 200 ||
+            statusResponse == 201 ||
+            statusResponse == 202 ||
+            statusResponse == 206 ||
+            statusResponse == 401 ||
+            statusResponse == 403 ||
+            statusResponse == 404 ||
+            statusResponse == 400 ||
+            statusResponse == 422) {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      } else {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      }
+    } on DioException catch (e) {
+      throw e.response!.statusMessage.toString();
+    }
+  }
+
+  Future<List<dynamic>> delete (String url, String body) async {
+    try {
+      final response = await dio.delete(
+        url,
+        data: body
+      );
+
+        final bodyResponse = response.data;
+        final int statusResponse = response.statusCode!;
+
+        debugPrint(bodyResponse.toString());
+        debugPrint(statusResponse.toString());
+
+        if (statusResponse == 200 ||
+            statusResponse == 201 ||
+            statusResponse == 202 ||
+            statusResponse == 206 ||
+            statusResponse == 401 ||
+            statusResponse == 403 ||
+            statusResponse == 404 ||
+            statusResponse == 400 ) {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      } else {
+        return [statusResponse, json.decode(json.encode(bodyResponse))];
+      }
+    } on DioException catch (e) {
+      throw e.response!.statusMessage.toString();
+    }
   }
 
   // Future<dynamic> postMultiPart(Uri url, Map<String, String> header, Map<String, String> form, XFile? file) async {
@@ -90,83 +288,4 @@ class NetworkUtils {
   //   }
   // }
 
-  Future<List<dynamic>> put(
-      Uri url, Map<String, String> header, String body) async {
-    return http.put(url, headers: header, body: body).then((response) {
-      final String bodyResponse = response.body;
-      final int statusResponse = response.statusCode;
-
-      debugPrint(bodyResponse);
-      debugPrint(statusResponse.toString());
-
-      if (statusResponse == 200 ||
-          statusResponse == 201 ||
-          statusResponse == 202 ||
-          statusResponse == 206 ||
-          statusResponse == 401 ||
-          statusResponse == 403 ||
-          statusResponse == 404 ||
-          statusResponse == 400 ||
-          statusResponse == 422) {
-        return [statusResponse, json.decode(bodyResponse)];
-      } else {
-        throw bodyResponse;
-      }
-    }).catchError((dynamic error) {
-      throw Exception(error);
-    });
-  }
-
-  Future<List<dynamic>> patch(
-      Uri url, Map<String, String> header, String body) async {
-    return http.patch(url, headers: header, body: body).then((response) {
-      final String bodyResponse = response.body;
-      final int statusResponse = response.statusCode;
-
-      debugPrint(bodyResponse);
-      debugPrint(statusResponse.toString());
-
-      if (statusResponse == 200 ||
-          statusResponse == 201 ||
-          statusResponse == 202 ||
-          statusResponse == 206 ||
-          statusResponse == 401 ||
-          statusResponse == 403 ||
-          statusResponse == 404 ||
-          statusResponse == 400 ||
-          statusResponse == 422) {
-        return [statusResponse, json.decode(bodyResponse)];
-      } else {
-        throw bodyResponse;
-      }
-    }).catchError((dynamic error) {
-      throw Exception(error);
-    });
-  }
-
-  Future<List<dynamic>> delete(
-      Uri url, Map<String, String> header, String body) async {
-    return http.delete(url, headers: header, body: body).then((response) {
-      final String bodyResponse = response.body;
-      final int statusResponse = response.statusCode;
-
-      debugPrint(bodyResponse);
-      debugPrint(statusResponse.toString());
-
-      if (statusResponse == 200 ||
-          statusResponse == 201 ||
-          statusResponse == 202 ||
-          statusResponse == 206 ||
-          statusResponse == 401 ||
-          statusResponse == 403 ||
-          statusResponse == 404 ||
-          statusResponse == 400) {
-        return [statusResponse, json.decode(bodyResponse)];
-      } else {
-        throw bodyResponse;
-      }
-    }).catchError((dynamic error) {
-      throw Exception(error);
-    });
-  }
 }
