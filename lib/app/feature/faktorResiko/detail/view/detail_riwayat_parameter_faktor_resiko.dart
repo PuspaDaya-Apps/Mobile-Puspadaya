e9e24d40ec7941be79635837c5faeb87d1e5c6b1
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-02-22 13:48:05 +0700

    fix: fixing faktor resiko

---
+++ lib/app/feature/faktorResiko/detail/view/detail_riwayat_parameter_faktor_resiko.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:puspadaya/app/view/widget/appbar_widget.dart';
+import '../model/get_detail_riwayat_faktor_resiko_model.dart'
+    as GetDetailRiwayatFaktorResiko;
+
+class DetailRiwayatParameterFaktorResiko extends StatelessWidget {
+  final GetDetailRiwayatFaktorResiko.FaktorResiko data;
+  const DetailRiwayatParameterFaktorResiko({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PrimaryAppBar(
+        title: data.namaFaktorResiko,
+        background: Colors.white,
+        onBackPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Judul Pertanyaan
+                Text(
+                  data.pertanyaan[0].namaPertanyaan,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 12),
+
+                // Gambar Pertanyaan
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    data.gambar,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 200,
+                  ),
+                ),
+                SizedBox(height: 14),
+                Text(
+                  'Jawaban Anda :',
+                  style: TextStyle(fontSize: 14),
+                ),
+                SizedBox(height: 16),
+                // List Jawaban
+                Column(
+                  children: data.pertanyaan[0].pilihanPertanyaan.map((jawaban) {
+                    bool isSelected = data.pertanyaan[0].jawaban
+                        .any((jwb) => jwb.pilihanPertanyaan.id == jawaban.id);
+
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 12),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: isSelected ? Colors.blue : Colors.grey,
+                          width: 1.5,
+                        ),
+                        color: isSelected
+                            ? Colors.blue.withOpacity(0.2)
+                            : Color(0xFFFAFAFA),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                      child: Text(
+                        jawaban.namaPilihan,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: isSelected ? Colors.blue : Colors.black,
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/feature/faktorResiko/detail/view/riwayat_parameter_faktor_resiko.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:puspadaya/app/feature/faktorResiko/detail/bloc/detail_faktor_resiko_bloc.dart';
+
+import '../../../../../config/theme/pallet_color.dart';
+import '../../../../../config/theme/shadow.dart';
+import '../../../../../utils/logger/logger.dart';
+import '../../../../view/widget/alert_dialog_petunjuk_faktor_resiko.dart';
+import '../../../../view/widget/appbar_widget.dart';
+import '../../../../view/widget/parameter_faktor_resiko_item.dart';
+import '../../../../view/widget/primary_button_widget.dart';
+import '../faktor_resiko_lahir_kembar.dart';
+import 'detail_riwayat_parameter_faktor_resiko.dart';
+
+class RiwayatParameterFaktorResiko extends StatelessWidget {
+  final String anakId;
+  const RiwayatParameterFaktorResiko({super.key, required this.anakId});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => DetailFaktorResikoBloc(),
+      child: RiwayatParameterFaktorResikoView(
+        anakId: anakId,
+      ),
+    );
+  }
+}
+
+class RiwayatParameterFaktorResikoView extends StatefulWidget {
+  final String anakId;
+  const RiwayatParameterFaktorResikoView({super.key, required this.anakId});
+
+  @override
+  State<RiwayatParameterFaktorResikoView> createState() =>
+      _RiwayatParameterFaktorResikoViewState();
+}
+
+class _RiwayatParameterFaktorResikoViewState
+    extends State<RiwayatParameterFaktorResikoView> {
+  @override
+  void initState() {
+    logger.d(widget.anakId);
+    context
+        .read<DetailFaktorResikoBloc>()
+        .add(FetchDetailFaktorResiko(id: widget.anakId));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PrimaryAppBar(
+        title: 'Faktor Resiko Permasalahan Gizi',
+        background: Colors.white,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 16), // Add some margin
+            child: GestureDetector(
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialogPetunjuk();
+                  },
+                );
+              },
+              child: Icon(
+                FontAwesomeIcons.circleExclamation, // Font Awesome icon
+                color: greenPrimaryMain, // Icon color
+                size: 24, // Icon size
+              ),
+            ),
+          ),
+        ],
+        onBackPressed: () {
+          Navigator.pop(context, true);
+        },
+      ),
+      backgroundColor: backgroundWhite10,
+      body: BlocBuilder<DetailFaktorResikoBloc, DetailFaktorResikoState>(
+        builder: (context, state) {
+          if (state is DetailFaktorResikoLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (state is DetailFaktorResikoFailed) {
+            return Center(
+              child: Text('Error Detail Faktor Resiko ${state.message}'),
+            );
+          }
+          if (state is DetailFaktorResikoSuccess) {
+            return ListView.builder(
+              itemCount: state.data.data.faktorResiko.length,
+              itemBuilder: (context, index) {
+                final parameter = state.data.data.faktorResiko[index];
+
+                return Padding(
+                  padding: const EdgeInsets.only(
+                      bottom: 5, top: 5, left: 16, right: 16),
+                  child: ParameterFaktorResikoItem(
+                    isDone: false,
+                    status: true,
+                    judul: parameter.namaFaktorResiko,
+                    keterangan: parameter.keterangan,
+                    terakhirDiisi: parameter.lastCompleted,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return DetailRiwayatParameterFaktorResiko(
+                              data: parameter,
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    isRiwayat: true,
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
