7c3caaecc5f9527b680d2c713006f0ce0c0ae6c7
Arilex <arilex36@gmail.com>
2025-03-23 14:03:22 +0700

    feature detail create kunjungan ibu hamil

--- lib/app/feature/kunjunganIbuHamil/detailCreateKunjungan/bloc/detailCreateKunjunganIbuHamilBloc/detail_create_kunjungan_ibu_hamil_bloc.dart
+++ lib/app/feature/kunjunganIbuHamil/detailCreateKunjungan/bloc/detailCreateKunjunganIbuHamilBloc/detail_create_kunjungan_ibu_hamil_bloc.dart
@@ -1,13 +1,46 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../../../../utils/shared_preferences_utils/shared_preferences_utils.dart';
+import '../../model/detail_create_kunjungan_ibu_hamil_response_model.dart';
+import '../../service/detail_create_kunjungan_ibu_hamil_api.dart';
+
 part 'detail_create_kunjungan_ibu_hamil_event.dart';
 part 'detail_create_kunjungan_ibu_hamil_state.dart';
 
 class DetailCreateKunjunganIbuHamilBloc extends Bloc<DetailCreateKunjunganIbuHamilEvent, DetailCreateKunjunganIbuHamilState> {
   DetailCreateKunjunganIbuHamilBloc() : super(DetailCreateKunjunganIbuHamilInitial()) {
-    on<DetailCreateKunjunganIbuHamilEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<DetailCreateKunjunganIbuHamilEvent>((event, emit) {});
+
+    on<GetDetailKunjunganIbuHamil>(getDetailKunjunganIbuHamil);
+  }
+
+  Future<void> getDetailKunjunganIbuHamil (GetDetailKunjunganIbuHamil event, Emitter<DetailCreateKunjunganIbuHamilState> emit) async {
+    emit(DetailCreateKunjunganIbuHamilProccessState());
+
+    String? accessToken = await SharedPrefUtils().getAccessToken();
+
+    if(accessToken == null) {
+      emit(DetailCreateKunjunganIbuHamilTokenExpiredState());
+    } else {
+      try {
+        List<dynamic> response = await DetailCreateKunjunganIbuHamilApi().getDetailIbuHamil(accessToken, event.idKunjungan);
+
+        int statusCode = response[0] as int;
+        
+
+        if(statusCode == 200) {
+          final DetailCreateKunjunganIbuHamilResponseModel detailCreateKunjunganIbuHamilResponseModel = DetailCreateKunjunganIbuHamilResponseModel.fromJson(response[1]);
+          emit(DetailCreateKunjunganIbuHamilSuccessState(detailCreateKunjunganIbuHamilResponseModel));
+        } else if (statusCode == 401) {
+          emit(DetailCreateKunjunganIbuHamilTokenExpiredState());
+        } else {
+          final ErrorMessage message = ErrorMessage.fromJson(response[1]);
+          emit(DetailCreateKunjunganIbuHamilFailedState(message.message));
+        }
+      } catch (error) {
+        emit(DetailCreateKunjunganIbuHamilFailedState(error.toString()));
+      }
+    }
   }
 }
--- lib/app/feature/kunjunganIbuHamil/detailCreateKunjungan/view/detail_create_kunjungan_ibu_hamil_screen.dart
+++ lib/app/feature/kunjunganIbuHamil/detailCreateKunjungan/view/detail_create_kunjungan_ibu_hamil_screen.dart
@@ -10,6 +10,8 @@ import 'package:puspadaya/config/screen_config/size_config.dart';
 import 'package:puspadaya/config/theme/pallet_color.dart';
 import 'package:puspadaya/config/theme/text_style.dart';
 
+import '../../../../../route/route_name.dart';
+import '../../../../view/screen/error_server_screen.dart';
 import '../bloc/detailCreateKunjunganIbuHamilBloc/detail_create_kunjungan_ibu_hamil_bloc.dart';
 
 class DetailCreateKunjunganIbuHamil extends StatelessWidget {
@@ -26,28 +28,31 @@ class DetailCreateKunjunganIbuHamil extends StatelessWidget {
 }
 
 class DetailCreateKunjunganIbuHamilView extends StatefulWidget {
-  const DetailCreateKunjunganIbuHamilView({super.key, required this.idKunjungan});
+  const DetailCreateKunjunganIbuHamilView(
+      {super.key, required this.idKunjungan});
   final String idKunjungan;
 
   @override
-  State<DetailCreateKunjunganIbuHamilView> createState() => _DetailCreateKunjunganIbuHamilViewState();
+  State<DetailCreateKunjunganIbuHamilView> createState() =>
+      _DetailCreateKunjunganIbuHamilViewState();
 }
 
-class _DetailCreateKunjunganIbuHamilViewState extends State<DetailCreateKunjunganIbuHamilView> {
+class _DetailCreateKunjunganIbuHamilViewState
+    extends State<DetailCreateKunjunganIbuHamilView> {
   Timer? _timer;
   int _seconds = 0;
   bool _isRunning = false;
 
   // Format waktu menjadi MM:SS
   String get formattedTime {
-     // int minutes = _seconds ~/ 60;
+    // int minutes = _seconds ~/ 60;
     // int seconds = _seconds % 60;
 
     int h = _seconds ~/ 3600;
     int m = ((_seconds - h * 3600)) ~/ 60;
     int s = _seconds - (h * 3600) - (m * 60);
 
-    if( h != 0) {
+    if (h != 0) {
       return '${h.toString().padLeft(2, '0')}:${m.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
     } else {
       return '${m.toString().padLeft(2, '0')}:${s.toString().padLeft(2, '0')}';
@@ -62,11 +67,12 @@ class _DetailCreateKunjunganIbuHamilViewState extends State<DetailCreateKunjunga
         _seconds = DateTime.now().difference(mulai).inSeconds;
       });
     });
-  } 
+  }
 
   @override
   void initState() {
     super.initState();
+    BlocProvider.of<DetailCreateKunjunganIbuHamilBloc>(context).add(GetDetailKunjunganIbuHamil(widget.idKunjungan));
   }
 
   @override
@@ -87,208 +93,217 @@ class _DetailCreateKunjunganIbuHamilViewState extends State<DetailCreateKunjunga
       ),
       backgroundColor: backgroundWhite10,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.all(20),
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Image(
-                    height: 240,
-                    image: AssetImage(
-                      imageOnTheWay,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        alignment: Alignment
-                            .center, // Pusatkan konten di dalam Container
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment
-                              .center, // Pusatkan elemen di dalam Row
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Jarak",
-                                  style: AppTextStyles.secoundaryTextMedium
-                                      .copyWith(
-                                    fontSize: 12,
-                                  ),
-                                ),
-                                SizedBox(
-                                    width:
-                                        4), // Tambahkan jarak antar teks jika diperlukan
-                                Text(
-                                  "2.5",
-                                  style:
-                                      AppTextStyles.primaryTextMedium.copyWith(
-                                    fontSize: 28,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: 38,
-                      color: Colors.black54,
-                      width: 2,
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment
-                              .center, // Pusatkan elemen di dalam Row
-                          children: [
-                            Text(
-                              "Waktu",
-                              style:
-                                  AppTextStyles.secoundaryTextMedium.copyWith(
-                                fontSize: 12,
-                              ),
-                            ),
-                            SizedBox(
-                                width:
-                                    4), // Tambahkan jarak antar teks jika diperlukan
-                            Text(
-                              formattedTime,
-                              style: AppTextStyles.primaryTextMedium.copyWith(
-                                fontSize: 28,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Container(
+        child: BlocConsumer<DetailCreateKunjunganIbuHamilBloc, DetailCreateKunjunganIbuHamilState>(
+          listener: (context, state) {
+            debugPrint(state.toString());
+          },
+          builder: (context, state) {
+            if(state is DetailCreateKunjunganIbuHamilProccessState) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  color: bluePrimaryMain,
+                )
+              );
+            }
+            if(state is DetailCreateKunjunganIbuHamilSuccessState) {
+              if(_isRunning == false) {
+                _toggleTimer(state.listDataIbuHamil.mulaiPada);
+              }
+              return SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.all(20),
                   width: double.infinity,
-                  height: 2,
-                  color: Colors.black54,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                const Text(
-                  textAlign: TextAlign.start,
-                  'NIK',
-                  style: TextStyle(
-                    fontSize: 12,
+                  padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Image(
+                          height: 240,
+                          image: AssetImage(
+                            imageOnTheWay,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              alignment: Alignment.center, // Pusatkan konten di dalam Container
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center, // Pusatkan elemen di dalam Row
+                                children: [
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        "Jarak",
+                                        style: AppTextStyles.secoundaryTextMedium.copyWith(
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      SizedBox(width:4), // Tambahkan jarak antar teks jika diperlukan
+                                      Text(
+                                        state.listDataIbuHamil.jarakTotal,
+                                        style: AppTextStyles.primaryTextMedium
+                                            .copyWith(
+                                          fontSize: 28,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 38,
+                            color: Colors.black54,
+                            width: 2,
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment
+                                    .center, // Pusatkan elemen di dalam Row
+                                children: [
+                                  Text(
+                                    "Waktu",
+                                    style: AppTextStyles.secoundaryTextMedium
+                                        .copyWith(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  SizedBox(width: 4), // Tambahkan jarak antar teks jika diperlukan
+                                  Text(
+                                    formattedTime,
+                                    style:
+                                        AppTextStyles.primaryTextMedium.copyWith(
+                                      fontSize: 28,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 2,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      const Text(
+                        textAlign: TextAlign.start,
+                        'NIK',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.nik),
+                      SizedBox(height: SizeConfig.calHeightMultiplier(16)),
+                      const Text(
+                        textAlign: TextAlign.start,
+                        'Nama Ibu Hamil',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.namaIbu),
+                      SizedBox(height: SizeConfig.calHeightMultiplier(16)),
+                      const Text(
+                        textAlign: TextAlign.start,
+                        'Alamat',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      Row(
+                        spacing: 8,
+                        children: [
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.dusun.desaKelurahan.kecamatan.kabupatenKota.namaKabupatenKota),
+                          ),
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.dusun.desaKelurahan.kecamatan.namaKecamatan),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      Row(
+                        spacing: 8,
+                        children: [
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.dusun.desaKelurahan.namaDesaKelurahan),
+                          ),
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.dusun.namaDusun),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      Row(
+                        spacing: 8,
+                        children: [
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.rt),
+                          ),
+                          Expanded(
+                            child: InfoFieldWidget(text: state.listDataIbuHamil.ibuHamil!.ibuAnak.rw),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(8),
+                      ),
+                      InfoFieldWidget(
+                        text: state.listDataIbuHamil.ibuHamil!.ibuAnak.alamat,
+                      ),
+                      SizedBox(
+                        height: SizeConfig.calHeightMultiplier(20),
+                      ),
+                      ButtonPrimary(
+                        color: bluePrimaryMain,
+                        mainButtonMessage: 'Selesai' , // Teks tombol
+                        mainButton: () {
+                          Navigator.pushNamed(context, FORM_TUGAS_IBU_HAMIL_KUNJUNGAN, arguments: state.listDataIbuHamil.id);
+                        }, // Jalankan timer
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                InfoFieldWidget(text: '354635284658310023'),
-                SizedBox(height: SizeConfig.calHeightMultiplier(16)),
-                const Text(
-                  textAlign: TextAlign.start,
-                  'Nama Ibu Hamil',
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                InfoFieldWidget(text: 'Citra Dewi'),
-                SizedBox(height: SizeConfig.calHeightMultiplier(16)),
-                const Text(
-                  textAlign: TextAlign.start,
-                  'Alamat',
-                  style: TextStyle(
-                    fontSize: 12,
-                  ),
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                Row(
-                  spacing: 8,
-                  children: [
-                    Expanded(
-                      child: InfoFieldWidget(text: 'Banyuwangi'),
-                    ),
-                    Expanded(
-                      child: InfoFieldWidget(text: 'Kabat'),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                Row(
-                  spacing: 8,
-                  children: [
-                    Expanded(
-                      child: InfoFieldWidget(text: 'Kabat'),
-                    ),
-                    Expanded(
-                      child: InfoFieldWidget(text: 'Bunder'),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                InfoFieldWidget(text: 'Dadapan'),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                Row(
-                  spacing: 8,
-                  children: [
-                    Expanded(
-                      child: InfoFieldWidget(text: '11'),
-                    ),
-                    Expanded(
-                      child: InfoFieldWidget(text: '03'),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(8),
-                ),
-                InfoFieldWidget(
-                  text: 'Jl. Jendaral Sudirman, Gg. Jembatan Ayu',
-                ),
-                SizedBox(
-                  height: SizeConfig.calHeightMultiplier(20),
-                ),
-                ButtonPrimary(
-                  color: _isRunning
-                      ? bluePrimaryMain
-                      : greenPrimaryMain, // Warna tombol
-                  mainButtonMessage:
-                      _isRunning ? 'Selesai' : 'Mulai', // Teks tombol
-                  mainButton: () {}, // Jalankan timer
-                ),
-              ],
-            ),
-          ),
+              );
+            }
+            return const ErrorServerScreen();
+          },
         ),
       ),
     );
