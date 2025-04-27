da71e84a8beabb9ab1873541f3b2ecb28f346f67
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-04-27 10:40:40 +0700

    fix : change version to 0.2.1

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -65,7 +65,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 padding:
                   EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
                 child: Text(
-                  "V. 1.0",
+                  "V. 0.2.1",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: SizeConfig.calMultiplierText(18),
