c691f3e5cbd1f10d6e5eef6a010dabbaf9a52793
Arilex <arilex36@gmail.com>
2025-11-18 23:05:47 +0700

    handle image link error di fitur update kunjungan rumah

--- lib/app/feature/kunjunganAnakTidakHadir/perbaruiKunjungan/view/perbarui_kunjungan_anak_tidak_hadir_screen.dart
+++ lib/app/feature/kunjunganAnakTidakHadir/perbaruiKunjungan/view/perbarui_kunjungan_anak_tidak_hadir_screen.dart
@@ -663,11 +663,12 @@ class _UploadImageState extends State<UploadImage> {
                       }
                     },
                     errorBuilder: (context, error, stackTrace) {
-                      return Icon(
-                        Icons.warning_amber_rounded,
-                        size: MediaQuery.sizeOf(context).width / 2,
-                        color: Colors.red,
-                      );
+                      return SizedBox.shrink();
+                      // return Icon(
+                      //   Icons.warning_amber_rounded,
+                      //   size: MediaQuery.sizeOf(context).width / 2,
+                      //   color: Colors.red,
+                      // );
                     },
                     image: NetworkImage(
                       widget.linkImages[index],
