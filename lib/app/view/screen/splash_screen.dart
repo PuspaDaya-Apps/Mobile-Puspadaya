d7525c47004bd1152b3bb37390fc7cebcf5e7a3f
Arilex <arilex36@gmail.com>
2025-12-01 12:51:12 +0700

    update version

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -68,7 +68,7 @@ class _SplashScreenState extends State<SplashScreen> {
                 padding:
                     EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
                 child: Text(
-                  "V. 2.5.4",
+                  "V. 2.5.5",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: SizeConfig.calMultiplierText(18),
