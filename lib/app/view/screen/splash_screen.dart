08a55ed69bce8094540f823e08cc73e1dc4a7f1e
Arilex <arilex36@gmail.com>
2025-12-10 11:32:44 +0700

    update splash screen

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -71,7 +71,8 @@ class _SplashScreenState extends State<SplashScreen> {
                 padding:
                     EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
                 child: Text(
-                  "Staging V. 2.5.5",
+                  // "Staging V. 2.5.5", //staging
+                  "V. 2.5.5", //Production
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: SizeConfig.calMultiplierText(18),
