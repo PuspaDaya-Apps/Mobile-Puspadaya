62d4cbeb576244e5b1359c0d1259f7c5e3287fa3
Arilex <arilex36@gmail.com>
2025-12-22 18:04:45 +0700

    penambahan maps handel di fitur create register orang tua

--- lib/app/feature/createRegisterOrangTua/model/post_orang_tua_body.dart
+++ lib/app/feature/createRegisterOrangTua/model/post_orang_tua_body.dart
@@ -159,6 +159,9 @@ class Ibu {
   @JsonKey(name: "jenis_disabilitas")
   final List<JenisDisabilitas> jenisDisabilitas;
 
+  double? latitude;
+  double? longitude;
+
   Ibu({
     required this.nomorKartuKeluarga,
     required this.nik,
@@ -175,6 +178,8 @@ class Ibu {
     this.tanggalMelahirkanSebelumnya,
     required this.jumlahAnak,
     required this.jenisDisabilitas,
+    this.latitude,
+    this.longitude
   });
 
   Ibu copyWith({
@@ -193,6 +198,8 @@ class Ibu {
     String? tanggalMelahirkanSebelumnya,
     int? jumlahAnak,
     List<JenisDisabilitas>? jenisDisabilitas,
+    double? latitude,
+    double? longitude
   }) =>
       Ibu(
         nomorKartuKeluarga: nomorKartuKeluarga ?? this.nomorKartuKeluarga,
@@ -207,10 +214,11 @@ class Ibu {
         nomorTelepon: nomorTelepon ?? this.nomorTelepon,
         golDarah: golDarah ?? this.golDarah,
         jenisKb: jenisKb ?? this.jenisKb,
-        tanggalMelahirkanSebelumnya:
-            tanggalMelahirkanSebelumnya ?? this.tanggalMelahirkanSebelumnya,
+        tanggalMelahirkanSebelumnya: tanggalMelahirkanSebelumnya ?? this.tanggalMelahirkanSebelumnya,
         jumlahAnak: jumlahAnak ?? this.jumlahAnak,
         jenisDisabilitas: jenisDisabilitas ?? this.jenisDisabilitas,
+        latitude: latitude ?? this.latitude,
+        longitude: longitude ?? this.longitude,
       );
 
   factory Ibu.fromJson(Map<String, dynamic> json) => _$IbuFromJson(json);
@@ -290,6 +298,8 @@ Ibu _$IbuFromJson(Map<String, dynamic> json) => Ibu(
       jenisDisabilitas: (json['jenis_disabilitas'] as List<dynamic>)
           .map((e) => JenisDisabilitas.fromJson(e as Map<String, dynamic>))
           .toList(),
+      latitude: json['latitude'] as double?,
+      longitude: json['longitude'] as double?
     );
 
 Map<String, dynamic> _$IbuToJson(Ibu instance) => <String, dynamic>{
@@ -308,4 +318,6 @@ Map<String, dynamic> _$IbuToJson(Ibu instance) => <String, dynamic>{
       'tanggal_melahirkan_sebelumnya': instance.tanggalMelahirkanSebelumnya,
       'jumlah_anak': instance.jumlahAnak,
       'jenis_disabilitas': instance.jenisDisabilitas,
+      'latitude': instance.latitude,
+      'longitude': instance.longitude,
     };
