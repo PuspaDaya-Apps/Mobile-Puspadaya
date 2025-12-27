5ecd31109cf19421c7f34d8b0a1eff803bbea229
Arilex <arilex36@gmail.com>
2025-12-27 11:04:18 +0700

    update model untuk maps handel fitur create kunjungan ibu hamil

--- lib/app/feature/kunjunganIbuHamil/listIbuHamil/model/create_kunjungan_ibu_hamil_model.dart
+++ lib/app/feature/kunjunganIbuHamil/listIbuHamil/model/create_kunjungan_ibu_hamil_model.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../../maps/model/lokasi.dart';
+
 // part 'create_kunjungan_ibu_hamil_model.g.dart';
 
 // @JsonSerializable()
@@ -12,12 +14,15 @@ class CreateKunjunganIbuHamilModel {
   String mulaiPada;
   @JsonKey(name: 'tanggal_kunjungan')
   DateTime tanggalKunjungan;
+  @JsonKey(name: 'lokasi_start')
+  Lokasi lokasiStart;
 
   CreateKunjunganIbuHamilModel({
     this.jenisKunjungan = "Ibu Hamil",
     required this.ibuHamilId,
     required this.mulaiPada,
-    required this.tanggalKunjungan
+    required this.tanggalKunjungan,
+    required this.lokasiStart
   });
 
   factory CreateKunjunganIbuHamilModel.fromJson(Map<String, dynamic> json) => _$CreateKunjunganIbuHamilModelFromJson(json);
@@ -32,6 +37,7 @@ CreateKunjunganIbuHamilModel _$CreateKunjunganIbuHamilModelFromJson(
       ibuHamilId: json['ibu_hamil_id'] as String,
       mulaiPada: json['mulai_pada'] as String,
       tanggalKunjungan: DateTime.parse(json['tanggal_kunjungan'] as String),
+      lokasiStart: Lokasi.fromJson(json['lokasi_start'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CreateKunjunganIbuHamilModelToJson(
@@ -41,4 +47,5 @@ Map<String, dynamic> _$CreateKunjunganIbuHamilModelToJson(
       'ibu_hamil_id': instance.ibuHamilId,
       'mulai_pada': instance.mulaiPada,
       'tanggal_kunjungan': instance.tanggalKunjungan.toIso8601String(),
+      'lokasi_start': instance.lokasiStart,
     };
