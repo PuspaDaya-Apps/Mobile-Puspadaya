a1e9190b2555286375c2fbfe2074253b7ece49bb
Arilex <arilex36@gmail.com>
2025-12-27 10:57:41 +0700

    penambah handle maps di fitur create kunjungan anak stunting

--- lib/app/feature/kunjunganAnakStunting/listAnakStunting/model/create_kunjungan_anak_stunting_model.dart
+++ lib/app/feature/kunjunganAnakStunting/listAnakStunting/model/create_kunjungan_anak_stunting_model.dart
@@ -1,5 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 
+import '../../../maps/model/lokasi.dart';
+
 // part 'create_kunjungan_anak_stunting_model.g.dart';
 
 // @JsonSerializable()
@@ -10,15 +12,17 @@ class CreateKunjunganAnakStuntingModel {
   String anakId;
   @JsonKey(name: 'mulai_pada')
   String mulaiPada;
-  // @JsonKey(name: 'j
   @JsonKey(name: 'tanggal_kunjungan')
   DateTime tanggalKunjungan;
+  @JsonKey(name: 'lokasi_start')
+  Lokasi lokasiStart;
 
   CreateKunjunganAnakStuntingModel({
     this.jenisKunjungan = "Anak Stunting",
     required this.anakId,
     required this.mulaiPada,
-    required this.tanggalKunjungan
+    required this.tanggalKunjungan,
+    required this.lokasiStart
   });
 
   factory CreateKunjunganAnakStuntingModel.fromJson(Map<String, dynamic> json) => _$CreateKunjunganAnakStuntingModelFromJson(json);
@@ -29,10 +33,11 @@ class CreateKunjunganAnakStuntingModel {
 CreateKunjunganAnakStuntingModel _$CreateKunjunganAnakStuntingModelFromJson(
         Map<String, dynamic> json) =>
     CreateKunjunganAnakStuntingModel(
-      jenisKunjungan: json['jenis_kunjungan'] as String,
+      jenisKunjungan: json['jenis_kunjungan'] as String? ?? "Anak Stunting",
       anakId: json['anak_id'] as String,
       mulaiPada: json['mulai_pada'] as String,
       tanggalKunjungan: DateTime.parse(json['tanggal_kunjungan'] as String),
+      lokasiStart: Lokasi.fromJson(json['lokasi_start'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$CreateKunjunganAnakStuntingModelToJson(
@@ -42,4 +47,5 @@ Map<String, dynamic> _$CreateKunjunganAnakStuntingModelToJson(
       'anak_id': instance.anakId,
       'mulai_pada': instance.mulaiPada,
       'tanggal_kunjungan': instance.tanggalKunjungan.toIso8601String(),
+      'lokasi_start': instance.lokasiStart,
     };
