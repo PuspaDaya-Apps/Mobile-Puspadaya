10bf4235210e90feea50a8cf25dcf2c3a63d68aa
Arilex <arilex36@gmail.com>
2026-02-08 09:46:42 +0700

    update version for production

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -49,9 +49,9 @@ class _SplashScreenState extends State<SplashScreen> {
       },
       child: Scaffold(
         //Production
-        // backgroundColor: bluePrimaryMain,
+        backgroundColor: bluePrimaryMain,
         //Staging
-        backgroundColor: greenPrimary50,
+        // backgroundColor: greenPrimary50,
         body: Stack(
           fit: StackFit.expand,
           children: [
@@ -70,8 +70,8 @@ class _SplashScreenState extends State<SplashScreen> {
               child: Padding(
                 padding: EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
                 child: Text(
-                  "Staging V. 2.7.3", //staging
-                  // "V. 2.7.3", //Production
+                  // "Staging V. 2.7.3", //staging
+                  "V. 2.7.3", //Production
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: SizeConfig.calMultiplierText(18),
