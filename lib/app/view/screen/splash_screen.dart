fc445439a6435d896af9bc794764749918e9ccff
Arilex <arilex36@gmail.com>
2026-02-18 12:26:52 +0700

    update version for staging

--- lib/app/view/screen/splash_screen.dart
+++ lib/app/view/screen/splash_screen.dart
@@ -70,8 +70,8 @@ class _SplashScreenState extends State<SplashScreen> {
               child: Padding(
                 padding: EdgeInsets.only(bottom: SizeConfig.calHeightMultiplier(25)),
                 child: Text(
-                  "Staging V. 2.7.3", //staging
-                  // "V. 2.7.3", //Production
+                  "Staging V. 2.7.4", //staging
+                  // "V. 2.7.4", //Production
                   textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: SizeConfig.calMultiplierText(18),
