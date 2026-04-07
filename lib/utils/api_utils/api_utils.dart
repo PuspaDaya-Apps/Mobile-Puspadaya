26d1d5b73cf32077ad85dd4f239c44ea58aa7bfc
Arilex <arilex36@gmail.com>
2026-04-07 14:52:08 +0700

    update link API Production

--- lib/utils/api_utils/api_utils.dart
+++ lib/utils/api_utils/api_utils.dart
@@ -6,7 +6,7 @@ class ApiUtils {
   //!Base URL
 
   // final String _baseUrl = "http://rkc8kowwc8og4ggo4ww0kw4o.103.109.210.102.sslip.io"; //! staging
-  final String _baseUrl = "http://ssc80wssow48gsgwwg8888s4.103.109.210.102.sslip.io"; //! production
+  final String _baseUrl = "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io"; //! production
 
   // final String _baseUrl = "https://40vg7x7b-8080.asse.devtunnels.ms";
   // final String _baseUrlQuaryParameter = "now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io";
